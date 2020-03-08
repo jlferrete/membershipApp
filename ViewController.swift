2c30c2853c8642e205b066a2f5d3d61db06b546e
JlFerrete <joseluisferrete@gmail.com>
2020-03-08 16:29:00 +0100

    Initial project

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MembershipApp
+//
+//  Created by Jose Luis Ferrete on 08/03/2020.
+//  Copyright © 2020 Jose Luis Ferrete. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
